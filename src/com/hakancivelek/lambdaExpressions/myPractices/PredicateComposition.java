132deb564069d6e3908584a3cc0fc4992282a006
Hakan CİVELEK <hakancivelek5461@gmail.com>
2025-02-19 23:05:06 +0300

    updated lambda expressions and saw how it's worked using the sout

--- src/com/hakancivelek/lambdaExpressions/myPractices/PredicateComposition.java
+++ src/com/hakancivelek/lambdaExpressions/myPractices/PredicateComposition.java
@@ -9,34 +9,59 @@ public class PredicateComposition {
         int num3 = -10;
         int num4 = -11;
 
-        Predicate<Integer> isEven = num -> num % 2 == 0;
-        Predicate<Integer> isPositive = num -> num > 0;
-        Predicate<Integer> isNegative = num -> num < 0;
+        Predicate<Integer> isEven = num -> {
+            System.out.println("isEven");
+            return num % 2 == 0;
+        };
+
+        Predicate<Integer> isPositive = num -> {
+            System.out.println("isPositive");
+            return num > 0;
+        };
+
+        Predicate<Integer> isNegative = num -> {
+            System.out.println("isNegative");
+            return num < 0;
+        };
 
         System.out.println(isEven.and(isPositive).test(10));
+        System.out.println();
         System.out.println(isEven.and(isPositive).test(11));
+        System.out.println();
         System.out.println(isEven.and(isPositive).test(-10));
+        System.out.println();
         System.out.println(isEven.and(isPositive).test(-11));
 
-        System.out.println();
+        System.out.println("\n-----------------\n");
 
         System.out.println(isEven.and(isNegative).test(10));
+        System.out.println();
         System.out.println(isEven.and(isNegative).test(11));
+        System.out.println();
         System.out.println(isEven.and(isNegative).test(-10));
+        System.out.println();
         System.out.println(isEven.and(isNegative).test(-11));
 
-        System.out.println();
+        System.out.println("\n-----------------\n");
 
-        System.out.println(isEven.or(isPositive).test(10));
-        System.out.println(isEven.or(isPositive).test(11));
-        System.out.println(isEven.or(isPositive).test(-10));
-        System.out.println(isEven.or(isPositive).test(-11));
-
+        System.out.println("isEvenOrIsPositive:\n");
+        Predicate<Integer> isEvenOrIsPositive = isEven.or(isPositive);
+        System.out.println(isEvenOrIsPositive.test(10));
         System.out.println();
+        System.out.println(isEvenOrIsPositive.test(11));
+        System.out.println();
+        System.out.println(isEvenOrIsPositive.test(-10));
+        System.out.println();
+        System.out.println(isEvenOrIsPositive.test(-11));
+
+        System.out.println("\n-----------------\n");
 
         System.out.println(isEven.or(isNegative).test(10));
+        System.out.println();
         System.out.println(isEven.or(isNegative).test(11));
+        System.out.println();
         System.out.println(isEven.or(isNegative).test(-10));
+        System.out.println();
         System.out.println(isEven.or(isNegative).test(-11));
     }
 }
